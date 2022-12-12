98023576b2d4ee3b1ca5ef1a656e70378963494c
quadri salawudeen <olamiquadri1@gmail.com>
2022-12-12 13:49:45 +0100

    commt

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Hero from './Components/Hero/Hero';
+// import MovieLists from './Components/MovieList/MovieLists';
+import Nav from './Components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Hero/>
+      {/* <MovieLists/> */}
+     {/* install axios  ....npm install axios*/}
+     
+    </div>
+  );
+}
+
+export default App;
